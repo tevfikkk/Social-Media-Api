833a15aa8ddda24bb9cb6f4de2a01eb25ae8241f
tevfik <tevfikhoglu@gmail.com>
2022-07-05 22:20:53 +0300

    comment

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -94,3 +94,88 @@ commentRouter.post('/:id', async (req: Request, res: Response) => {
 
   res.status(201).json(newComment)
 })
+
+// @route   PUT api/comment/:id
+// @desc    Update a comment
+// @access  Private
+commentRouter.put('/:id', async (req: Request, res: Response) => {
+  // Get the user from the request body
+  const { comment }: { comment: string } = req.body
+
+  // content is required
+  if (!comment) {
+    return res.status(400).json({
+      error: 'Please provide comment'
+    })
+  }
+
+  // Check if cookie is set
+  if (!req.cookies.token) {
+    return res.status(401).json({
+      error: 'You must be logged in to update a comment'
+    })
+  }
+
+  // Check if comment exists
+  const ifCommentExists = await prisma.comment.findUnique({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  if (!ifCommentExists) {
+    return res.status(404).json({
+      error: 'Comment not found'
+    })
+  }
+
+  // Update the comment
+  const updatedComment = await prisma.comment
+    .update({
+      where: {
+        id: req.params.id // Comment id
+      },
+      data: {
+        comment
+      }
+    })
+    .catch((err: Error) => res.status(500).json({ error: err }))
+
+  res.status(200).json(updatedComment)
+})
+
+// @route   DELETE api/comment/:id
+// @desc    Delete a comment
+// @access  Private
+commentRouter.delete('/:id', async (req: Request, res: Response) => {
+  // Check if cookie is set
+  if (!req.cookies.token) {
+    return res.status(401).json({
+      error: 'You must be logged in to delete a comment'
+    })
+  }
+
+  // Check if comment exists
+  const ifCommentExists = await prisma.comment.findUnique({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  if (!ifCommentExists) {
+    return res.status(404).json({
+      error: 'Comment not found'
+    })
+  }
+
+  // Delete the comment
+  const deletedComment = await prisma.comment
+    .delete({
+      where: {
+        id: req.params.id // Comment id
+      }
+    })
+    .catch((err: Error) => res.status(500).json({ error: err }))
+
+  res.status(200).json(deletedComment)
+})
